bfa9835ceaad1cd551df4afca42c4fc6bd6f9403
Satoshi Tanaka <overthere555@gmail.com>
2023-04-27 01:26:32 +0900

    Mix pico-sdk and embedded-hal

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
 #![no_std]
 
-use panic_abort as _;
+use panic_halt as _;
 use core::ffi::{c_int, c_char};
+use rp2040_hal as hal;
+use hal::pac;
+use embedded_hal::digital::v2::OutputPin;
 
 extern {
     fn stdio_init_all() -> bool;
@@ -11,12 +14,21 @@ extern {
 
 #[no_mangle]
 pub extern "C" fn main() {
+    let mut pac = pac::Peripherals::take().unwrap();
+    let sio = hal::Sio::new(pac.SIO);
+    let pins = hal::gpio::Pins::new(pac.IO_BANK0, pac.PADS_BANK0, sio.gpio_bank0, &mut pac.RESETS);
+    let mut led_pin = pins.gpio25.into_push_pull_output();
+
+    // stdio has to be initialized after initializing pins with Rust HAL
     unsafe { stdio_init_all(); }
 
     loop {
-        unsafe {
-            puts("Hello, world\0".as_ptr() as *const c_char);
-            sleep_ms(1000);
-        }
+        unsafe { puts("Hello, world\0".as_ptr() as *const c_char); }
+
+        led_pin.set_high().unwrap();
+        unsafe { sleep_ms(500); }
+
+        led_pin.set_low().unwrap();
+        unsafe { sleep_ms(500); }
     }
 }
