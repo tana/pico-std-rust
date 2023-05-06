03391028cd7df2444f34ca9c0ed51d2524a276c9
Satoshi Tanaka <overthere555@gmail.com>
2023-05-07 03:26:24 +0900

    (WIP) Add rp2040-hal

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@
 
 use std::{thread, time::Duration, sync::{Mutex, Arc}};
 use core::{ffi::{c_char, c_void, c_int}};
+use rp2040_hal as hal;
+use hal::{pac as pac, Sio};
+use embedded_hal::digital::v2::OutputPin;
 
 extern {
     fn stdio_init_all() -> bool;
@@ -20,6 +23,33 @@ extern {
 }
 
 extern "C" fn main_task(_: *mut c_void) {
+    let mut peripherals = pac::Peripherals::take().unwrap();
+    let sio = Sio::new(peripherals.SIO);
+    let pins = hal::gpio::Pins::new(
+        peripherals.IO_BANK0,
+        peripherals.PADS_BANK0,
+        sio.gpio_bank0,
+        &mut peripherals.RESETS
+    );
+
+    // Somehow, stdio has to be initialized after Peripherals::take().
+    unsafe { stdio_init_all(); }
+
+    let mut pin = pins.gpio2.into_push_pull_output();
+
+    // Pin toggling thread
+    thread::spawn(move || {
+        loop {
+            pin.set_high().unwrap();
+            thread::sleep(Duration::from_millis(500));
+
+            pin.set_low().unwrap();
+            thread::sleep(Duration::from_millis(500));
+        }
+    });
+
+    // Test of synchronization mechanisms and stdio
+
     let counter = Arc::new(Mutex::new(0));
 
     {
@@ -54,8 +84,6 @@ pub extern "C" fn main() {
     unsafe {
         esp_newlib_locks_init();
         esp_pthread_init();
-
-        stdio_init_all();
 
         xTaskCreate(
             main_task, "main_task\0".as_ptr() as *const c_char, 1024,
